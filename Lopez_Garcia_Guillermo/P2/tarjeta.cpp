41561f2b420f00746ee24cdc7aec54c93bb5f17a
guilogar <jdkdejava@gmail.com>
2018-04-26 11:59:31 +0200

    Mejoras.

--- Lopez_Garcia_Guillermo/P2/tarjeta.cpp
+++ Lopez_Garcia_Guillermo/P2/tarjeta.cpp
@@ -3,6 +3,15 @@
 
 #include "tarjeta.hpp"
 
+Tarjeta::Tarjeta(Tipo t, Numero n, Usuario *u, Fecha f) : tipo_(t), numero_(n), usuario_(u) {
+    if(f < Fecha())
+        throw Caducada(f);
+}
 
+Tarjeta::~Tarjeta() {
+    
+    if(usuario_ != nullptr)
+        usuario_->no_es_titular_de(*this);
+}
 
 #endif
--- Lopez_Garcia_Guillermo/P2/tarjeta.hpp
+++ Lopez_Garcia_Guillermo/P2/tarjeta.hpp
@@ -5,6 +5,8 @@
 #include "../P1/cadena.hpp"
 #include "usuario.hpp"
 
+#include <cstring>
+
 class Usuario;
 
 class Numero {
@@ -14,7 +16,9 @@ class Numero {
             return num_troq_.c_str();
         }
         
-        bool operator <(const Numero& n);
+        bool operator <(const Numero& n) {
+            return std::stod(num_troq_.c_str()) < std::stod((const char*)n);
+        }
         
         enum Razon { LONGITUD, DIGITOS, NO_VALIDO };
         class Incorrecto {
@@ -30,13 +34,34 @@ class Numero {
 
 class Tarjeta {
     public:
-        //Tipo tipo() const;
-        Numero numero() const;
-        Fecha caducidad() const;
-        Cadena titular_facial() const;
-        const Usuario* titular() const;
-    private:
+        enum Tipo { VISA, Mastercard, Maestro, JCB, AmericanExpress };
         
+        Tarjeta(Tipo t, Numero n, Usuario *u, Fecha f);
+        Tipo tipo() const { return tipo_; }
+        Numero numero() const { return numero_; }
+        Fecha caducidad() const { return caducidad_; }
+        Cadena titular_facial() const { return titular_facial_; }
+        const Usuario* titular() const { return usuario_; }
+        
+        Tarjeta(const Tarjeta& t) = delete;
+        Tarjeta& operator =(const Tarjeta& t) = delete;
+        
+        void anular_titular() { usuario_ = nullptr; }
+        
+        class Caducada {
+            public:
+                Caducada(Fecha f) : f_(f) { }
+                Fecha cuando() const { return f_; }
+            private:
+                Fecha f_;
+        };
+        ~Tarjeta();
+    private:
+        Tipo tipo_;
+        Numero numero_;
+        Usuario* usuario_;
+        Fecha caducidad_;
+        Cadena titular_facial_;
 };
 
 #endif
